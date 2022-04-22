80f4a27724b1099e7d9ccc3dbec83437d7b3c142
kaittli <kaitlynli@ucla.edu>
2022-04-21 17:38:55 -0700

    week 3 lab assignment

--- week3/js/init.js
+++ week3/js/init.js
@@ -1,14 +1,15 @@
 // declare the map
-const map = L.map('the_map').setView([34.0709,-118.444], 5);
+const map = L.map('the_map').setView([45.0709,-65.888], 3);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-addMarker(37,-122,'home','home land!')
-addMarker(32,-118,'work','where i work land!')
-addMarker(39,-119,'location 1','random location')
-addMarker(36,-120,'location 2','another random location')
+// addMarker(37,-122,'home','home land!')
+// addMarker(32,-118,'work','where i work land!')
+// addMarker(39,-119,'location 1','random location')
+// addMarker(36,-120,'location 2','another random location')
+
 
 // create a function to add markers
 function addMarker(lat,lng,title,message){
@@ -17,24 +18,39 @@ function addMarker(lat,lng,title,message){
     createButtons(lat,lng,title)
     return message
 }
-function createButtons(latitude, longitude,title){
-    const newButton = document.createElement("button"); 
-    newButton.id = "button"+title; 
-    newButton.innerHTML = "this is button is:" + title; 
-    newButton.setAttribute("lat",latitude); 
-    newButton.setAttribute("lng",longitude); 
-    newButton.addEventListener('click', function(){
-        map.flyTo([latitude,longitude]); 
-    })
-    document.getElementById("contents").appendChild(newButton);
-}
+// function createButtons(latitude, longitude,title){
+//     const newButton = document.createElement("button"); 
+//     newButton.id = "button"+title; 
+//     newButton.innerHTML = "this is button is:" + title; 
+//     newButton.setAttribute("lat",latitude); 
+//     newButton.setAttribute("lng",longitude); 
+//     newButton.addEventListener('click', function(){
+//         map.flyTo([latitude,longitude]); 
+//     })
+//     document.getElementById("contents").appendChild(newButton);
+// }
 fetch("map.geojson")
-    .then(function (data){
-        return data.json()
+    .then(response =>{
+        return response.json()
     })
-    .then(function (data){
+    .then(data =>{
         // Basic Leaflet method to add GeoJSON data
-        L.geoJSON(data).addTo(map)
+        L.geoJSON(data,{
+            pointToLayer: (feature, latlng) => { 
+                return L.circleMarker(latlng, {color: feature.properties.color})
+            }
+        }).bindPopup(layer => {
+            return layer.feature.properties.place;
+        }).addTo(map)
     });
 
-    console.log(hello)
+    // console.log(hello)
+    // .then(function (data){
+    //     return data.json()
+    // })
+    // .then(function (data){
+    //     // Basic Leaflet method to add GeoJSON data
+    //     L.geoJSON(data).addTo(map)
+    // });
+
+    // console.log(hello)
--- week3/js/part1.js
+++ week3/js/part1.js
@@ -1,17 +1,20 @@
 // declare the map
-let mapoptions= {'center': [34.0709,-118.444], 'zoom': 5}
-const map = L.map('the_map').setView(mapoptions.center, mapoptions.zoom);
-console.log(mapoptions)
+const map = L.map('the_map').setView([37.5709, -121.888], 9.5);
+// let mapoptions= {'center': [34.609,-118.444], 'zoom': 5}
+// console.log(mapoptions)
+//const map = L.map('the_map').setView([37.5709, -121.888], 9); 
 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-addMarker(37,-122,'home','home land!')
-addMarker(32,-118,'work','where i work land!')
-addMarker(39,-119,'location 1','random location')
-addMarker(36,-120,'location 2','another random location')
+addMarker(37.40441852494992, -122.11735385658818,'Boba Bliss', 'Family-owned business that uses only nonartifical ingredients for boba drinks')
+addMarker(37.74321168022817, -122.47332396837658, 'Mr Szechuan', 'For the spicy/Szechuan food lovers craving traditional dishes in a modern setting')
+addMarker(37.661719260092376, -121.87261797393313, 'Meadowlark Dairy','Drive-thru softserve spot with rotating seasonal flavors')
+addMarker(37.455978559249026, -121.9097665622934, 'RawASF', 'Plant based cafe offering vegan burgers, pizza, and (my fav) customizable acai bowls')
+addMarker(37.330615660037694, -121.87927994920632,'Cali Spartan tacos', 'Small shop that serves amazing crispy tacos')
+addMarker(37.32058116404451, -122.03109125858902, 'XLB Kitchen', 'Cute spot serving soup dumplings + other Chinese dishes—ran by a super hardworking owner')
 
 
 // create a function to add markers
@@ -24,7 +27,7 @@ function addMarker(lat,lng,title,message){
 function createButtons(latitude, longitude,title){
     const newButton = document.createElement("button"); 
     newButton.id = "button"+title; 
-    newButton.innerHTML = "this is button is:" + title; 
+    newButton.innerHTML = title; 
     newButton.setAttribute("lat",latitude); 
     newButton.setAttribute("lng",longitude); 
     newButton.addEventListener('click', function(){
