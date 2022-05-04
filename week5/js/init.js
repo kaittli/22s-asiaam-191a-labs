f5d1d419b6a9ac532454b313dad02a17d86f6c2c
kaittli <kaitlynli@ucla.edu>
2022-05-04 00:11:46 -0700

    week5 lab

---
+++ week5/js/init.js
@@ -0,0 +1,40 @@
+// declare variables
+let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+
+// use the variables
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    return message
+}
+
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRIHr3uHd7EcxlEEW1sQLev1UarBfMhcdlV4u5lPM-SDsuqsnjhIwoqM8GmUQoMO-G6MxvY1a6MpgVR/pub?output=csv" 
+
+function loadData(url){
+    Papa.parse(url, {
+        header: true,
+        download: true,
+        complete: results => processData(results)
+    })
+}
+
+function processData(results){
+    console.log(results) //for debugging: this can help us see if the results are what we want
+    results.data.forEach(data => {
+        console.log(data) // for debugging: are we seeing each data correctly?
+        addMarker(data.lat,data.lng,data['Please name the most recent location where you have received medical attention during your time at UCLA (full address).'],data['What type of medical care are you sharing your experience about?'])
+    })
+}
+
+// we will use this later!
+loadData(dataUrl)
+
+
+    
